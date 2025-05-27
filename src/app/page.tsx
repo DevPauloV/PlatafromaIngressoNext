253aa2bc1f6781b60e26b2643602b7416e8e0db2
DevPauloV <vitorcanalle@gmail.com>
2025-05-27 13:07:55 -0300

    feat: Implement event display layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,58 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <h1>Hello word</h1>
-    
+    <main className="container mx-auto py-8 px-4">
+      <h1 className="text=3xl font-bold mb-8"> Shows disponiveis</h1>
+      <div className="grid grid-cols-3 gap-8">
+        <div className="bg-white shadow-lg rounded0lg">
+          <img src="" alt="" />
+          <div className="p-4">
+            <h2 className="text-xl text-black font-bold"> Show do Radiohead</h2>
+            <p className="text-gray-700 mt-2">
+              Sab, 15/06/2024
+            </p>
+            <p className="text-gray-700 mt-2">Tem 10 lugares disponiveis</p>
+            <p className="text-gray-700 mt-2">R$ 500</p>
+            <p>
+              <button>Reservar Lugar</button>
+            </p>
+          </div>
+        </div>
+
+
+        <div className="bg-white shadow-lg rounded0lg">
+          <img src="" alt="" />
+          <div className="p-4">
+            <h2 className="text-xl text-black font-bold"> Show do Radiohead</h2>
+            <p className="text-gray-700 mt-2">
+              Sab, 15/06/2024
+            </p>
+            <p className="text-gray-700 mt-2">Tem 10 lugares disponiveis</p>
+            <p className="text-gray-700 mt-2">R$ 500</p>
+            <p>
+              <button>Reservar Lugar</button>
+            </p>
+          </div>
+        </div>
+
+
+        <div className="bg-white shadow-lg rounded0lg">
+          <img src="" alt="" />
+          <div className="p-4">
+            <h2 className="text-xl text-black font-bold"> Show do Radiohead</h2>
+            <p className="text-gray-700 mt-2">
+              Sab, 15/06/2024
+            </p>
+            <p className="text-gray-700 mt-2">Tem 10 lugares disponiveis</p>
+            <p className="text-gray-700 mt-2">R$ 500</p>
+            <p>
+              <button>Reservar Lugar</button>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </main>
   );
 }
